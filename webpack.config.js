960e5c42b738de71fb5c4f97a5e64a2d35f969f3
Dmitry Kuznetsov <dimushka5.93@mail.ru>
2022-01-13 17:22:35 +0300

    webpack + babel + react + typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,37 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index-bundle.js"
+    entry: "./src/index.tsx",
+    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+    mode: process.env.NODE_ENV || "development",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"]
+                use: ["babel-loader"],
             },
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ]
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
 };
